2613751feee87f9678a8854bd0eb8a227b4e821e
William Eichelberger <wceichelb@gmail.com>
2017-01-25 16:22:30 -0800

    change tmp dir structure to organize by sid

--- add_code_columns_etl.rb
+++ add_code_columns_etl.rb
@@ -1,21 +1,21 @@
 require "reindeer-etl"
 
-sid = 237884
+sid = 692985
 
 pre_process do
   first_run = true
-  CSV.foreach("./tmp/vvexport_#{sid}.csv", {col_sep: "\t", quote_char: "|"}) do |row|
+  CSV.foreach("./tmp/#{sid}/vvexport_#{sid}.csv", {col_sep: "\t", quote_char: "|"}) do |row|
     if first_run
       first_run = false
       if row.select{ |r| !r.nil? }.any?{|r| r.include?(" ")}
-        system( "sed -i '1d' './tmp/vvexport_#{sid}.csv'")
+        system( "sed -i '1d' './tmp/#{sid}/vvexport_#{sid}.csv'")
       end
     end
   end
 end
 
-source(ReindeerETL::Sources::CSVSource, "./tmp/vvexport_#{sid}.csv", {col_sep: "\t", quote_char: "|"})
+source(ReindeerETL::Sources::CSVSource, "./tmp/#{sid}/vvexport_#{sid}.csv", {col_sep: "\t", quote_char: "|"})
 
-transform(ReindeerETL::Transforms::ResponseStatus, "./tmp/limesurvey_survey_#{sid}.txt", {except: ["id", "token", "submitdate", "lastpage", "startlanguage"], complete: true})
+transform(ReindeerETL::Transforms::ResponseStatus, "./tmp/#{sid}/limesurvey_survey_#{sid}.txt", {except: ["startdate", "datestamp", "id", "token", "submitdate", "lastpage", "startlanguage", "ipaddr"], complete: true})
 
-destination(ReindeerETL::Destinations::CSVDest, "./tmp/step1_#{sid}.csv")
+destination(ReindeerETL::Destinations::CSVDest, "./tmp/#{sid}/step1_#{sid}.csv")
--- add_token_etl.rb
+++ add_token_etl.rb
@@ -1,23 +1,24 @@
 require "reindeer-etl"
 
-sid = 237884
+sid = 692985
 
 pre_process do
   first_run = true
-  CSV.foreach("./tmp/vvexport_#{sid}.csv", {col_sep: "\t", quote_char: "|"}) do |row|
+  CSV.foreach("./tmp/#{sid}/vvexport_#{sid}.csv", {col_sep: "\t", quote_char: "|"}) do |row|
     if first_run
       first_run = false
       if row.select{ |r| !r.nil? }.any?{|r| r.include?(" ")}
-        system( "sed -i '1d' './tmp/vvexport_#{sid}.csv'")
+        system( "sed -i '1d' './tmp/#{sid}/vvexport_#{sid}.csv'")
       end
     end
   end
 end
 
 source(ReindeerETL::Sources::MultiSource, "token",
-       ["./tmp/step1_#{sid}.csv", "./tmp/tokens_#{sid}.csv"],
-       target_cols: [["GradYear", "LotProgramId", "LotProgramAlias", "ABFMLastFour", "LotProgramCivilian", "LotProgramYrs"]],
+       ["./tmp/#{sid}/step1_#{sid}.csv", "./tmp/#{sid}/tokens_#{sid}.csv"],
+       target_cols: [["GradYear", "LotProgramId", "LotProgramAlias", "AbfmLastFour", "LotProgramCivilian", "LotProgramYrs"]],
+       # target_cols: [["LotProgramId", "LotProgramAlias", "LotProgramCivilian", "LotProgramYrs"]],
        namespace: "LotGradYr3"
       )
 
-destination(ReindeerETL::Destinations::CSVDest, "./tmp/recoded_#{sid}.csv", {col_sep: "\t"})
+destination(ReindeerETL::Destinations::CSVDest, "./tmp/#{sid}/recoded_#{sid}.csv", {col_sep: "\t"})
